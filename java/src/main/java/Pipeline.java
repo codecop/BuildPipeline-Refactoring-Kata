81f779299c2942d8f33bbd4537e0b4f41c1c7570
Peter Kofler <peter.kofler@code-cop.org>
2020-06-18 19:35:33 +0200

    Move 3 handle methods in classes.

--- java/src/main/java/Pipeline.java
+++ java/src/main/java/Pipeline.java
@@ -19,75 +19,85 @@ public class Pipeline {
 
     public void run(Project project) {
         Map<String, Boolean> results = new HashMap<>();
-        handleTests(project, results);
-        handleDeployment(project, results);
-        handleEmail(project, results);
+        new TestStep().handleTests(project, results);
+        new DeployStep().handleDeployment(project, results);
+        new EmailStep().handleEmail(project, results);
     }
 
-    private void handleTests(Project project, Map<String, Boolean> results) {
-        boolean testsPassed;
-        if (project.hasTests()) {
-            testsPassed = runTests(project);
-        } else {
-            log.info("No tests");
-            testsPassed = true;
-        }
-        results.put("testsPassed", testsPassed);
-    }
+    class TestStep {
 
-    private void handleDeployment(Project project, Map<String, Boolean> results) {
-        boolean deploySuccessful;
-        if (results.get("testsPassed")) {
-            deploySuccessful = deploy(project);
-        } else {
-            deploySuccessful = false;
-        }
-        results.put("deploySuccessful", deploySuccessful);
-    }
-
-    private void handleEmail(Project project, Map<String, Boolean> results) {
-        if (config.sendEmailSummary()) {
-            sendEmails(results.get("testsPassed"), results.get("deploySuccessful"));
-        } else {
-            log.info("Email disabled");
-        }
-    }
-
-    private boolean runTests(Project project) {
-        boolean testsPassed;
-        if ("success".equals(project.runTests())) {
-            log.info("Tests passed");
-            testsPassed = true;
-        } else {
-            log.error("Tests failed");
-            testsPassed = false;
-        }
-        return testsPassed;
-    }
-
-    private boolean deploy(Project project) {
-        boolean deploySuccessful;
-        if ("success".equals(project.deploy())) {
-            log.info("Deployment successful");
-            deploySuccessful = true;
-        } else {
-            log.error("Deployment failed");
-            deploySuccessful = false;
-        }
-        return deploySuccessful;
-    }
-
-    private void sendEmails(boolean testsPassed, boolean deploySuccessful) {
-        log.info("Sending email");
-        if (testsPassed) {
-            if (deploySuccessful) {
-                emailer.send("Deployment completed successfully");
+        public void handleTests(Project project, Map<String, Boolean> results) {
+            boolean testsPassed;
+            if (project.hasTests()) {
+                testsPassed = runTests(project);
             } else {
-                emailer.send("Deployment failed");
+                log.info("No tests");
+                testsPassed = true;
             }
-        } else {
-            emailer.send("Tests failed");
+            results.put("testsPassed", testsPassed);
         }
+
+        private boolean runTests(Project project) {
+            boolean testsPassed;
+            if ("success".equals(project.runTests())) {
+                log.info("Tests passed");
+                testsPassed = true;
+            } else {
+                log.error("Tests failed");
+                testsPassed = false;
+            }
+            return testsPassed;
+        }
+
     }
 
+    class DeployStep {
+
+        public void handleDeployment(Project project, Map<String, Boolean> results) {
+            boolean deploySuccessful;
+            if (results.get("testsPassed")) {
+                deploySuccessful = deploy(project);
+            } else {
+                deploySuccessful = false;
+            }
+            results.put("deploySuccessful", deploySuccessful);
+        }
+
+        private boolean deploy(Project project) {
+            boolean deploySuccessful;
+            if ("success".equals(project.deploy())) {
+                log.info("Deployment successful");
+                deploySuccessful = true;
+            } else {
+                log.error("Deployment failed");
+                deploySuccessful = false;
+            }
+            return deploySuccessful;
+        }
+
+    }
+
+    class EmailStep {
+
+        public void handleEmail(Project project, Map<String, Boolean> results) {
+            if (config.sendEmailSummary()) {
+                sendEmails(results.get("testsPassed"), results.get("deploySuccessful"));
+            } else {
+                log.info("Email disabled");
+            }
+        }
+
+        private void sendEmails(boolean testsPassed, boolean deploySuccessful) {
+            log.info("Sending email");
+            if (testsPassed) {
+                if (deploySuccessful) {
+                    emailer.send("Deployment completed successfully");
+                } else {
+                    emailer.send("Deployment failed");
+                }
+            } else {
+                emailer.send("Tests failed");
+            }
+        }
+    }
 }
